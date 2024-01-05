cebaf9e842be4e616f75f09189148fc18632ef36
Y79S84M <yostm.dev@gmail.com>
2024-01-05 16:44:24 +0100

    getting ambitious. starting to implement packages installation depending on OS

--- install-homebrew-and-packages.sh
+++ install-homebrew-and-packages.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # Create Dev directory and its sub-directories
+default_shell_was_bash_already=true
+bash_shell_is_listed=$(cat /etc/shells | grep -e /*/bash)
+bash_shell="/bin/bash"
 pwd=$(pwd)
 home_directory="/Users/$USER"
 dev_directory="$home_directory/Dev"
 dev_sub_directories=("/Php" "/Java" "/Bash")
+
+change_shell_to_bash() {
+    if [[ $bash_shell_is_listed ]] && [[ $(which $SHELL) != $bash_shell ]]
+    then
+	echo "================================"
+	echo "Changing $(which shell) to $bash_shell and it requires your password !"
+	echo
+	chsh -s $bash_shell -u $USER
+	default_shell_was_bash_already=false
+    fi
+}
 
 create_dev_subdirectories() {
     i=0
@@ -20,16 +34,54 @@ create_directories() {
     create_dev_subdirectories
 }
 
+create_dev_directories() {    
 if [[ $pwd == $home_directory ]]
 then
+    echo "================================"
     echo "Creating Dev directories"
+    echo
     create_directories
 else
+    echo "================================"
     echo "Changing to home directory"
     cd $home_directory
     echo "Creating Dev directories"
     create_directories
 fi
+}
+
+echo "================================"
+echo "--       INITIALISATION       --"
+echo "================================"
+echo 
+echo "Checking shell !"
+echo
+change_shell_to_bash
+
+create_dev_directories
+
+echo "================================"
+echo "--       Choose your OS       --"
+echo "================================"
+echo
+
+select os in Linux MacOS Exit; do
+    case $os in
+	Linux)
+	    echo "You choose linux"
+	    break ;;
+	MacOS)
+	    echo "You choose macos"
+            break ;;
+	Exit)
+	    echo "Exiting ..."
+	    break ;;
+	*)
+	    echo "You must type 1 or 2 in order to select your OS"
+	    ;;
+    esac
+done
+exit 0
 
 # installing xcode
 xcode-select --install
